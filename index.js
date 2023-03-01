0a4966a08230c255069199956da99e53157132fd
Ryan Morgan <Zerkyn.55@gmail.com>
2023-03-01 16:59:53 -0700

    changed message to display movie names

--- index.js
+++ index.js
@@ -7,24 +7,33 @@ const addMovie = (e) => {
     let movie = document.createElement('li')
     let movieTitle = document.createElement('span')
     movieTitle.textContent = inputField.value
-    movie.appendChild(movieTitle)
-    movieList.appendChild(movie)
     movieTitle.addEventListener('click', crossOfMovie)
+    movie.appendChild(movieTitle)
     let deleteBtn = document.createElement('button')
+    movieList.appendChild(movie)
     deleteBtn.textContent = 'X'
     deleteBtn.addEventListener('click', deleteMovie)
     movie.appendChild(deleteBtn)
-    inputField = []
+    inputField.value = ''
 }
 
 document.querySelector('form').addEventListener('submit', addMovie)
 
 const deleteMovie = e => {
     e.target.parentNode.remove()
-    message.textContent = 'Movie deleted!'
+    message.textContent = `${e.target.parentNode.textContent} deleted!`
+    revealMessage()
 }
 
 const crossOfMovie = e => {
     e.target.classList.toggle('checked')
-    e.target.classList.contains('checked') ? message.textContent = 'Movie watched!' : message.textContent = 'Movie added back!'
+    e.target.classList.contains('checked') ? message.textContent = `${e.target.textContent} watched!` : message.textContent = `${e.target.textContent} added back!`
+    revealMessage()
+}
+
+const revealMessage = () => {
+    message.classList.remove('hide')
+    setTimeout(() => {
+        message.classList.add('hide')
+    }, 1000)
 }
